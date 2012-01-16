f6df60db9a5dd8fc8291cbe172e772b805339fbd
Panagiotis Kosmidis <panos@hostindex.gr>
2012-01-16 09:19:51 +0200

    Additions & Changes
    - node_modules will not shipped any more with this repo, instead users are obliged to run "npm install" in order to get dependencies
    
    - /.htaccess (root) point's the domain on port 3000 for node.js in case under apache (nginx is preferable) Change the port to the port you will set for the app
    
    - Silex PHP removed, node.js now handles the API
    
    - Application is now wrapped with express.js
    
    - html5boilerplate's build is not working any more

---
+++ routes/api.js
@@ -0,0 +1,64 @@
+exports.get_users = function (req, res) {
+	var users = {
+		{
+			id	: 23,
+			name	: 'John Doe',
+			email_address	: 'john@doe.com',
+			country	: {
+				code	: 'en',
+				title	: 'USA'
+			},
+			language	: {
+				code	: 'en',
+				title	: 'English'
+			}
+		}, {
+			id	: 24,
+			name	: 'Panagiotis Kosmidis',
+			email_address	: 'info@aviant.av',
+			country	: {
+				code	: 'el',
+				title	: 'Hellas'
+			},
+			language	: {
+				code	: 'gr',
+				title	: 'Greek'
+			}
+		}
+	};
+	
+	res.json(users, 200);
+};
+
+exports.get_user = function (req, res) {
+	switch (parseInt(req.params.id)) {
+		case 23:
+			var user = {
+				id	: 23,
+				name	: 'John Doe',
+				email_address	: 'john@doe.com',
+				country	: {
+					code	: 'en',
+					title	: 'USA'
+				},
+				language	: {
+					code	: 'en',
+					title	: 'English'
+				}
+			};
+			break;
+
+		case 24:
+			var user = {
+				name	: 'Panos',
+				email_address	: 'info@aviant.av',
+				country	: {
+					code	: 'el',
+					title	: 'Hellas'
+				}
+			};
+			break;
+	}
+
+	res.json(user, 200);
+};
